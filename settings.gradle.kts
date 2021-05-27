589e29a394b7369943bc71a20a0622f5e3e61c74
diesieben07 <diesieben07@gmail.com>
2021-05-28 00:04:07 +0200

    Move plugins to settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,3 @@
-
 rootProject.name = "reinhardt"
 include("dbgen")
 include("core")
@@ -9,3 +8,11 @@ include("generator")
 include("model-state")
 include("class-names")
 include("jdbc")
+
+pluginManagement {
+    plugins {
+        kotlin("multiplatform") version "1.5.10" apply false
+        kotlin("jvm")version "1.5.10" apply false
+        kotlin("plugin.serialization") version "1.5.10" apply false
+    }
+}
